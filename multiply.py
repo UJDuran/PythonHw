acd6a1aa0fe8c99f1bdb3d91719925f8fbd87501
ulises <ulises.duran@gmail.com>
2021-10-13 20:56:57 -0700

    updating my multiply file. This file will multiply numbers given by the user The code will return false if the user input an invalid input

---
+++ multiply.py
@@ -0,0 +1,10 @@
+def multiply_list():
+	print ("Enter a list of numbers seperated by a space")
+	numList = [int(x) for x in input().split()]
+
+	product = 1
+
+	for i in numList:
+		product = product * i
+		if i == 0:
+		return bool(0)
