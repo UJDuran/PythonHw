3dab928f4ea181f9aec694c56cdcf560ddc7f25e
ulises <ulises.duran@gmail.com>
2021-10-13 21:04:28 -0700

    This is to update the multiply file. inditations were incorrect.

--- multiply.py
+++ multiply.py
@@ -1,10 +1,14 @@
 def multiply_list():
-	print ("Enter a list of numbers seperated by a space")
-	numList = [int(x) for x in input().split()]
+    print ("Enter a list of numbers seperated by a space")
+    numList = [int(x) for x in input().split()]
 
-	product = 1
+    product = 1
+    isNum = bool(1)
 
-	for i in numList:
-		product = product * i
-		if i == 0:
-		return bool(0)
+
+    for i in numList:
+        product = product * i
+        if i == 0:
+            return bool(0)
+
+    print (product)X
